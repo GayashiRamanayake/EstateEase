d6247ac6d50c8317b695f54070f82f6b045ab360
GayashiRamanayake <gayashi.20232620@iit.ac.lk>
2025-01-03 01:42:00 +0530

    header completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Routes, Route, BrowserRouter} from 'react-router-dom';
 import Home from './pages/Home';
 import Header from './components/Header';
+import Buy from './pages/Buy';
+import Rent from './pages/Rent';
 
 
 export default function App() {
@@ -9,6 +11,8 @@ export default function App() {
       <Header />
       <Routes>
         <Route path="/home" element={<Home />} />
+        <Route path="/buy" element={<Buy />} />
+        <Route path="/rent" element={<Rent />} />
       </Routes>   
     </BrowserRouter>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
 import './Header.css';
+import { Link } from 'react-router-dom';
 
 
 export default function Header() {
   return (
-    <header>
-      <h1>
-        <span>Estate</span>
-        <span>Ease</span>
-      </h1>
+    <header className='header'>
+      <div className="header-box">
+
+        <Link to='/home' className='link-list'>
+        <h1 className='header-logo'>
+          <span className='header-logo-first'>Estate</span>
+          <span className='header-logo-second'>Ease</span>
+        </h1>
+        </Link>
+
+        <ul className="header-list">
+          <Link to='/home' className='link-list'><li className="header-list-item">Home</li></Link>
+          <Link to='/buy' className='link-list'><li className="header-list-item">Buy</li></Link>
+          <Link to='/rent' className='link-list'><li className="header-list-item">Rent</li></Link>
+        </ul>
+
+      </div>
     </header>
 
 
   )
 }
-
