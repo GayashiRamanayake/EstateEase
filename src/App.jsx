46b4ecbec8e7c1d0233130bf11b291f1ed5d7e14
GayashiRamanayake <gayashi.20232620@iit.ac.lk>
2025-01-02 17:58:50 +0530

    create header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import { Routes, Route, BrowserRouter} from 'react-router-dom';
+import Home from './pages/Home';
+import Header from './components/Header';
+
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/home" element={<Home />} />
+      </Routes>   
+    </BrowserRouter>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Header.css';
+
+
+export default function Header() {
+  return (
+    <header>
+      <h1>
+        <span>Estate</span>
+        <span>Ease</span>
+      </h1>
+    </header>
+
+
+  )
+}
+
